6dbbf0fec1e4a0062a379e221a1bd46c4ae4a9bc
David Goldfarb <deg@degel.com>
2013-10-09 12:12:28 +0300

    Some sorely-need comments and readme.

--- README.md
+++ README.md
@@ -3,9 +3,47 @@ muxx
 
 Deploy multiple production and dev websites on one instance of compojure.
 
+The Muxx library wraps around Compojure to give an easy way to deploy
+multiple small web apps to a single server.
+
+_Note: I've designed muxx for single-page JavaScript apps. It can certainly
+be generalized to deal with other kinds of web sites, but I've not yet
+given any thought to such directions._
+
+Goals:
+* Let each web app live in its own Clojure project
+* Create deployments that bundle multiple independent apps on a single server
+* Supported for real deployment as well as testing on localhost
+* Support for dev and production versions of each app
+* Support for ClojureScript and browser-repls, e.g. via [https://github.com/cemerick/austin](Austin).
+* Don't hide any of the underlying flexibility of compojure, ring, leiningen, etc.
+
+Motivations to use muxx include increased modularity and reduced cloud
+deployment costs for apps that are very lightly used. For example, I
+have several personal apps that I access only once every few days, but
+I want them each to be instantly available when I need them. It would
+be foolish to keep a VM instance running 24/7 for each one of them.
+
 ## Usage
 
-FIXME
+Configure your DNS to find all of your apps at the IP of your hosting service.
+
+It's also very useful to map "*.local.yourdomain.com" to 127.0.0.1
+(localhost). Then you can run production and dev versions of all your
+apps locally and simultaneously:
+* `app1.local.yourdomain.com` is the production version of app1
+* `app1-dev.local.yourdomain.com` is the dev version of app1
+* `app2.local.yourdomain.com` is the production version of app2
+* `app2-dev.local.yourdomain.com` is the dev version of app2
+* etc.
+
+Each web app is registered by calling degel.muxx.server/add-app.
+
+Then, you need to write a deployment function that calls,
+e.g. run-jetty on muxx/app.
+
+(Note: this is still undergoing rapid evolution. I hope to have a much
+cleaner interface very soon).
 
 ## License
 
--- src/degel/muxx/server.clj
+++ src/degel/muxx/server.clj
@@ -1,3 +1,15 @@
+;;; Copyright (c) 2013, David Goldfarb. All rights reserved.
+;;; Contact info: deg@degel.com
+;;;
+;;; The use and distribution terms for this software are covered by the Eclipse
+;;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can
+;;; be found in the file epl-v10.html at the root of this distribution.
+;;; By using this software in any fashion, you are agreeing to be bound by the
+;;; terms of this license.
+;;;
+;;; You must not remove this notice, or any other, from this software.
+
+
 (ns degel.muxx.server
   (:require [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [resources not-found]]
@@ -5,46 +17,66 @@
             [shoreleave.middleware.rpc :refer [wrap-rpc]]
             [net.cgrand.enlive-html :as html]
             [compojure.handler :refer [site]]
-            [cemerick.austin.repls :refer (browser-connected-repl-js)]
+            [cemerick.austin.repls :as austin-repls]
             [degel.cljutil.devutils :as dev]))
 
 
+;;; All of the web apps we are handling.
+;;; Call app-app to add or modify.
+;;; [TODO] Do we need an API to remove a website?
 (def app-dispatch (atom {}))
 
-(defrecord app-data [name base-page production-js dev-js])
+(defrecord ^:private app-data
+  [name           ;; Name of a website
+   base-page      ;; base html page, for the production site.
+   production-js  ;; JavaScript to load on the production page.
+   dev-js         ;; JavaScript to load on the development page.
+   ])
 
-(defn add-app [& {:keys [name base-page production-js dev-js]}]
+
+(defn add-app
+  "Add an app to this deployment. Apps are keyed by name."
+  [& {:keys [name base-page production-js dev-js]}]
   (swap! app-dispatch
          assoc name (->app-data name base-page production-js dev-js)))
 
-(defn dev-page
+
+(defn- dev-page
+  "Create a development page from the production page. Currently, this
+   changes which JavaScript is run (typically to support debugging), and
+   injects Austin support for brower-repl access to the page."
   ([{:keys [base-page production-js dev-js]}]
      (dev-page (str "public" base-page) production-js dev-js))
   ([page production-js dev-js]
      ((html/template page []
-        [:body] (html/append (html/html [:script (browser-connected-repl-js)]))
+        [:body] (html/append (html/html [:script (austin-repls/browser-connected-repl-js)]))
         [[:script (html/attr= :src production-js)]] (html/set-attr :src dev-js)))))
 
 
-(defn find-site-records [server-request]
+(defn- find-site-records
+  "Match the server name to choose one of our apps"
+  [server-request]
    (filter (fn [{:keys [name] :as record}]
              (re-matches (re-pattern (str "(?i).*" name ".*")) server-request))
            (vals @app-dispatch)))
 
-(defn dev-site? [server-request {:keys [name] :as record}]
+(defn dev-site?
+  "Is the app name suffixed with '-dev'? If so, this is a request for the development site."
+  [server-request {:keys [name] :as record}]
   (string? (re-matches (re-pattern (str "(?i).*" name "-dev.*")) server-request)))
 
 
+;;; [TODO] Need informative and customizable error pages.
 (defroutes app-routes
   (GET "/" {:keys [server-name] :as all-keys}
     (let [[matching-site & extra-matches] (find-site-records server-name)]
-      (cond (nil? matching-site)        (not-found "<h1>David moans: 'app website not found'.</h1>")
-            extra-matches               (not-found "<h1>David moans: 'Ambiguous app website URL'.</h1>")
+      (cond (nil? matching-site)        (not-found "<h1>Muxx moans: 'app website not found'.</h1>")
+            extra-matches               (not-found "<h1>Muxx moans: 'Ambiguous app website URL'.</h1>")
             (dev-site? server-name
                        matching-site)   (dev-page matching-site)
             true                        (redirect (:base-page matching-site)))))
   (resources "/")   ;; to serve static pages saved in resources/public directory
-  (not-found "<h1>David moans: 'page not found'.</h1>"))
+  (not-found "<h1>Muxx moans: 'page not found'.</h1>"))
 
 
 (def app (-> app-routes wrap-rpc site))
