6a78cd884c0a59e0af16567a7c465c61b0a26ed2
David Goldfarb <deg@degel.com>
2013-11-25 00:53:49 +0200

    Use degel group-id and update dependencies.

--- project.clj
+++ project.clj
@@ -10,17 +10,14 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-(defproject muxx "0.1.3"
-  :description "DEPRECATED: Use degel/muxx. Deploy multiple production and dev websites on one instance of compojure."
+(defproject degel/muxx "0.1.4"
+  :description "Deploy multiple production and dev websites on one instance of compojure."
   :url "https://github.com/deg/muxx"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [;; Clojure itself
-                 [org.clojure/clojure "1.5.1"]
-
-                 ;; Degel's Clojure utility library
-                 [degel-clojure-utils "0.1.15"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [degel/degel-clojure-utils "0.1.17"]]
 
   :profiles {:dev
              { :plugins [[lein-marginalia "0.7.1"]
-                         [com.cemerick/austin "0.1.1"]]}})
+                         [com.cemerick/austin "0.1.3"]]}})
